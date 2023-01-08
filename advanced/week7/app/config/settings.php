fe734f92c72a61d8cc326767b9b5457af02c6d77
antwanvdm <antwanvdm@gmail.com>
2023-01-08 20:56:23 +0100

    db week 7 advanced minor stuff.

--- advanced/week7/app/config/settings.php
+++ advanced/week7/app/config/settings.php
@@ -11,6 +11,9 @@ const LOG_PATH = __DIR__ . '/../logs/';
 const INCLUDES_PATH = __DIR__ . '/../';
 
 //Custom error handler, so every error will throw a custom ErrorException
-set_error_handler(function (int $severity, string $message, string $file, int $line) {
+set_error_handler(function (int $severity, string $message, string $file, int $line): bool|null {
+    if ((error_reporting() & $severity) === 0) {
+        return null;
+    }
     throw new ErrorException($message, $severity, $severity, $file, $line);
 });
